edff1b01b37fa1a7db59c65538dd1e02172c5204
John Chen <john@soobb.com>
2023-06-02 01:50:12 -0500

    No speakers

--- transcribe.sh
+++ transcribe.sh
@@ -1,1 +1,8 @@
-python3 -m pyannote_whisper.cli.transcribe $1.ogg --model medium.en --language en --diarization True --output_format=TXT --hf_token $2 --num_speakers=$3
+
+if [ -z "$3" ]; then
+    num_speakers="None"
+else
+    num_speakers="$3"
+fi
+
+python3 -m pyannote_whisper.cli.transcribe $1.ogg --model medium.en --language en --diarization True --output_format=TXT --hf_token $2 --num_speakers=$num_speakers
